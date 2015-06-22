048ee3aff81d1312857312f7705501b286cdc9f9
Thomas <dktdn@hotmail.com>
2015-06-22 18:08:57 +0200

    Begyndt på en vejledning til appen.

---
+++ Alculator/app/src/main/java/com/example/helge/alculator/TutorialActivity.java
@@ -0,0 +1,14 @@
+package com.example.helge.alculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TutorialActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+
+    }
+}
